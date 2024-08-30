d151557b3987f6bedf5888d3516e4e839eaf729a
Azul-Torres-1108 <167554917+Azul-Torres-1108@users.noreply.github.com>
2024-08-30 12:20:50 -0600

    Add files via upload

---
+++ mivariables.py
@@ -0,0 +1,7 @@
+# variables
+print("Mis variables")
+
+edad = 17
+nombre= "Mederyth Azul"
+print("Edad", edad)
+print("Nombre", nombre)
---
+++ parafor.py
@@ -0,0 +1,5 @@
+# for en español es para
+print("Ciclo For de repeticion")
+minovias=["Lupita", "Ana", "Agripina"]
+for chicas in minovias:
+    print(chicas)
